4709abd31a4a72fda38a16529f3cb808cf99cba6
Matheus Alves <mtalves27@gmail.com>
2023-07-22 20:52:53 -0300

    chore: update search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,17 +2,14 @@ import { InputHTMLAttributes } from "react";
 
 import { Search as LucideSearch } from "lucide-react";
 
+import { Input } from "./ui/Input";
+
 interface SearchProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 export function Search({ ...props }: SearchProps) {
   return (
     <div className="relative">
-      <input
-        type="text"
-        placeholder="Buscar Eventos"
-        className="w-[470px] rounded-xl border border-interactive-secondary  pl-11 text-black transition-colors hover:bg-interactive-secondary/5 focus-visible:border-interactive-primary focus-visible:bg-interactive-secondary/5 focus-visible:ring-interactive-primary"
-        {...props}
-      />
+      <Input placeholder="Buscar eventos" className="w-[470px] pl-11" />
       <LucideSearch className="absolute top-1/2 ml-3 h-5 w-5 -translate-y-1/2 text-content-base" />
     </div>
   );
