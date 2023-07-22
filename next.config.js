0192f2271187f835d6cf66a7f8022cc8eff1f0b2
Matheus Alves <mtalves27@gmail.com>
2023-07-22 20:56:40 -0300

    chore: adjust redirect

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/events",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
