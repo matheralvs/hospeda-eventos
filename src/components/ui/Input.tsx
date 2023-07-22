7be4fd6505248e6f5ae15aae4c97013f6e308f9a
Matheus Alves <mtalves27@gmail.com>
2023-07-22 20:53:44 -0300

    chore: created input component

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,23 @@
+import { InputHTMLAttributes, forwardRef } from "react";
+
+import { cn } from "@/lib/utils";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        type="text"
+        className={cn(
+          "w-full rounded-xl border border-interactive-secondary text-black placeholder:text-content-base/40 hover:bg-interactive-secondary/0 focus-visible:border-interactive-primary focus-visible:bg-interactive-secondary/0 focus-visible:ring-interactive-primary",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+
+Input.displayName = "Input";
