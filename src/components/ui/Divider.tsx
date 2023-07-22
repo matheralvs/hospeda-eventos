9b22a42d561eba7a67f299351ff8709a91be9e2f
Matheus Alves <mtalves27@gmail.com>
2023-07-22 20:53:31 -0300

    chore: created divider component

--- src/components/ui/Divider.tsx
+++ src/components/ui/Divider.tsx
@@ -1,3 +1,15 @@
-export function Divider() {
-  return <div className="h-[1px] bg-layout-body" />;
-}
+import { HTMLAttributes, forwardRef } from "react";
+
+import { cn } from "@/lib/utils";
+
+interface DividerProps extends HTMLAttributes<HTMLDivElement> {}
+
+export const Divider = forwardRef<HTMLDivElement, DividerProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div className={cn("h-[1px] bg-layout-body", className)} ref={ref} />
+    );
+  },
+);
+
+Divider.displayName = "Divider";
