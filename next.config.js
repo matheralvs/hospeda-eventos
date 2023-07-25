8d715739fb8c0517a7448d5c9d12104f4614a074
Matheus Alves <mtalves27@gmail.com>
2023-07-24 21:55:01 -0300

    chore: added config

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/events",
-        permanent: false,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
