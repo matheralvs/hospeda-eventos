8ea873021b1ba538b6cd91432bcaf398f9f14688
Matheus Alves <mtalves27@gmail.com>
2023-07-24 21:57:25 -0300

    chore: added event listing

--- src/app/(dashboard)/events/page.tsx
+++ src/app/(dashboard)/events/page.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next";
 
+import { eventService } from "@/services/eventService";
+
 import { HeaderWithActions } from "@/components/HeadingWithActions";
 import { EventsTable } from "@/components/tables/EventsTable";
 
@@ -7,14 +9,25 @@ export const metadata: Metadata = {
   title: "Meus Eventos",
 };
 
-export default function Events() {
+export default async function Events() {
+  const events = await eventService.list();
+
   return (
     <main className="flex flex-col gap-6 px-20 pt-8">
       <HeaderWithActions />
 
       <EventsTable.Root>
         <EventsTable.Head />
-        <EventsTable.Body />
+        {events.map((event) => (
+          <EventsTable.Body
+            key={event.id}
+            name={event.name}
+            privacy={event.privacy}
+            city={event.city}
+            state={event.state}
+            initialDate={event.initialDate}
+          />
+        ))}
       </EventsTable.Root>
     </main>
   );
--- src/components/tables/EventsTable/EventsTableBody.tsx
+++ src/components/tables/EventsTable/EventsTableBody.tsx
@@ -2,17 +2,34 @@
 
 import Image from "next/image";
 
-import { IconButton } from "@/components/ui/buttons/IconButton";
-
 import * as Popover from "@radix-ui/react-popover";
+import dayjs from "dayjs";
 import { MoreHorizontal, Pencil, Trash2, Calendar, MapPin } from "lucide-react";
+
+import { IconButton } from "@/components/ui/buttons/IconButton";
 
 import { Badge } from "../../ui/Badge";
 import { Divider } from "../../ui/Divider";
 import { OptionsPopover } from "./components/OptionsPopover";
 
-export function EventsTableBody() {
+interface EventsTableBodyProps {
+  name: string;
+  privacy: "public" | "private";
+  city: string;
+  state: string;
+  initialDate: string;
+}
+
+export function EventsTableBody({
+  name,
+  privacy,
+  city,
+  state,
+  initialDate,
+}: EventsTableBodyProps) {
   const tdStyle = "whitespace-nowrap px-6 py-3 border-b border-layout-body";
+
+  const dateFormatted = dayjs(initialDate).format("DD/MM/YYYY");
 
   return (
     <tbody>
@@ -28,72 +45,23 @@ export function EventsTableBody() {
             />
 
             <div className="flex flex-col gap-4">
-              <h3 className="text-base font-bold">Nome do evento</h3>
+              <h3 className="text-base font-bold">{name}</h3>
               <div className="flex gap-6">
                 <div className="flex gap-1 text-xs text-content-base/60">
                   <Calendar className="h-4 w-4 text-primary-pure" />
-                  XX-XX Mês XXXX
+                  {dateFormatted}
                 </div>
 
                 <div className="flex gap-1 text-xs text-content-base/60">
                   <MapPin className="h-4 w-4 text-primary-pure" />
-                  Cidade/UF
+                  {`${city}/${state}`}
                 </div>
               </div>
             </div>
           </div>
         </td>
         <td className={tdStyle}>
-          <Badge />
-        </td>
-        <td className={tdStyle}>
-          <Popover.Root>
-            <Popover.Trigger asChild aria-controls="options">
-              <IconButton icon={MoreHorizontal} />
-            </Popover.Trigger>
-
-            <OptionsPopover.Root>
-              <OptionsPopover.Actions icon={Pencil} label="Editar" />
-              <Divider />
-              <OptionsPopover.Actions
-                icon={Trash2}
-                label="Excluir"
-                type="delete"
-              />
-            </OptionsPopover.Root>
-          </Popover.Root>
-        </td>
-      </tr>
-
-      <tr>
-        <td className={tdStyle}>
-          <div className="flex items-center gap-4">
-            <Image
-              src="https://images.unsplash.com/photo-1523580494863-6f3031224c94?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=50"
-              alt=""
-              width={500}
-              height={500}
-              className="h-20 w-[106px] rounded-xl"
-            />
-
-            <div className="flex flex-col gap-4">
-              <h3 className="text-base font-bold">Nome do evento</h3>
-              <div className="flex gap-6">
-                <div className="flex gap-1 text-xs text-content-base/60">
-                  <Calendar className="h-4 w-4 text-primary-pure" />
-                  XX-XX Mês XXXX
-                </div>
-
-                <div className="flex gap-1 text-xs text-content-base/60">
-                  <MapPin className="h-4 w-4 text-primary-pure" />
-                  Cidade/UF
-                </div>
-              </div>
-            </div>
-          </div>
-        </td>
-        <td className={tdStyle}>
-          <Badge type="private" />
+          <Badge type={privacy} />
         </td>
         <td className={tdStyle}>
           <Popover.Root>
